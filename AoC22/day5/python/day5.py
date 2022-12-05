ce654c6f5ca6a5f78267c4569f1602f858c14f7a
afroseppo <kobo-kobe@hotmail.com>
2022-12-05 15:44:35 +0200

    Day 5 python done

--- AoC22/day5/python/day5.py
+++ AoC22/day5/python/day5.py
@@ -1,4 +1,5 @@
 import re
+import copy
 
 def move(n, origin, destination, stacks):
 
@@ -8,8 +9,19 @@ def move(n, origin, destination, stacks):
 
     return stacks
 
-def movept2(n, origin, destionation, stacks):
-    pass
+def movept2(n, origin, destination, stacks_pt2):
+    to_be_moved = []
+
+    if n < len(stacks_pt2[origin - 1]):
+        to_be_moved.extend(stacks_pt2[origin - 1][-n:])
+        del stacks_pt2[origin - 1][-n:]
+    else:
+        to_be_moved.extend(stacks_pt2[origin - 1])
+        stacks_pt2[origin - 1].clear()
+
+    stacks_pt2[destination - 1].extend(to_be_moved)
+
+    return stacks_pt2
 
 with open('input.txt', 'r') as f:
     stacks_raw, raw_instructs = f.read().split('\n\n')
@@ -24,12 +36,21 @@ for line in stacks_raw.split('\n')[:-1]:
         if char != ' ':
             stacks[i].insert(0, char)
 
+stacks_pt2 = copy.deepcopy(stacks)
+
 for n, o, d in instructs:
     stacks = move(int(n), int(o), int(d), stacks)
+    stacks_pt2 = movept2(int(n), int(o), int(d), stacks_pt2)
 
 res = ''
+res_pt2 = ''
 
 for s in stacks:
     res += s[len(s) - 1]
 
+for s2 in stacks_pt2:
+    if len(s2) > 0:
+        res_pt2 += s2[len(s2) - 1]
+
 print(res)
+print(res_pt2)
