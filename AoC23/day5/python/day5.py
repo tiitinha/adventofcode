55d7234076a3eae5bf34c321ca1d8b7d71a88cad
Harri Tiitinen <htiitinen94@gmail.com>
2023-12-06 13:02:41 +0200

    Day 6 done

--- AoC23/day5/python/day5.py
+++ AoC23/day5/python/day5.py
@@ -1,15 +1,32 @@
 import re
 
-maps = {}
-
+maps = []
 with open('../input.txt') as f:
-    input_data = [x for x in f.read().split('\n\n')]
+    input_data = [x for x in f.read().strip().split('\n\n')]
     seeds = [int(x) for x in re.findall('([0-9]+)', input_data[0])]
+
     for input in input_data[1:]:
         mapping = input.split('\n')
-        name = mapping[0]
-        formatted_mapping = [x.split('\n') for x in mapping[1:]]
-        maps[name] = formatted_mapping
+        formatted_mapping = [z.split(' ') for y in [x.split('\n') for x in mapping[1:]] for z in y]
+        maps.append(formatted_mapping)
 
-print(seeds)
-print(maps)
+seeds = [int(x) for x in seeds]
+lowest_list = []
+
+for seed in seeds:
+    new_seed = seed
+    for map in maps:
+        for rng in map:
+            source_start = int(rng[1])
+            dest_start = int(rng[0])
+            rng_width = int(rng[2])
+
+            offset = dest_start - source_start
+
+            if source_start <= new_seed < source_start + rng_width:
+                new_seed = new_seed + offset
+                break
+
+    lowest_list.append(new_seed)
+
+print(min(lowest_list))
---
+++ AoC23/day6/python/day6.py
@@ -0,0 +1,41 @@
+import re
+import cmath
+import numpy as np
+import math
+
+with open('../input.txt') as f:
+    raw_input = f.readlines()
+
+    time = [int(x) for x in re.findall('([0-9]+)', raw_input[0])]
+    distance = [int(x) for x in re.findall('([0-9]+)', raw_input[1])]
+
+def calc_res(race_time, race_distance):
+    
+    discriminant = (race_time ** 2) - (4 * 1 * (race_distance))
+
+    sol_1 = math.floor((race_time - cmath.sqrt(discriminant)).real / (2 * 1))
+    sol_2 = math.ceil((race_time + cmath.sqrt(discriminant)).real / (2 * 1))
+
+    ans = sol_2 - sol_1 - 1
+    
+    return ans
+
+result = 1
+
+for race in range(len(time)):
+    possibilities = 0
+    race_time = time[race]
+    race_distance = distance[race]
+
+    ans = calc_res(race_time, race_distance)
+
+    result *= ans
+
+print(result)
+
+pt2_time = int(''.join([str(x) for x in time]))
+pt2_dist = int(''.join([str(x) for x in distance]))
+
+race_time_2 = calc_res(pt2_time, pt2_dist)
+
+print(race_time_2)
