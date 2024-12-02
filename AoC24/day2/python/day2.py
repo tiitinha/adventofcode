2d80f397786fedcdfce3a5fe509f0ec8ac6e8d8a
Harri Tiitinen <kobo-kobe@hotmail.com>
2024-12-02 13:17:20 +0200

    Update day2.py
    Small fixes, not complete though

--- AoC24/day2/python/day2.py
+++ AoC24/day2/python/day2.py
@@ -1,23 +1,20 @@
 import numpy as np
 
-with open('../test.txt') as f:
+with open('./input.txt') as f:
     levels = [[int(j) for j in i] for i in [x.strip().split(' ') for x in f.readlines()]]
 
 count = 0
 count2 = 0
 for level in levels:
     differences = np.diff(level)
+    diff2 = np.array([level[i] - level[i - 2] for i in range(2, len(level))])
     if ((np.all(differences >= 1) & np.all(differences <= 3)) | (np.all(differences <= -1) & np.all(differences >= -3))):
         count += 1
-    
-    level_len = len(differences)
-    masked_1 = differences[np.logical_and(differences >= 1, differences <= 3)]
-    masked_2 = differences[np.logical_and(differences <= -1, differences >= -3)]
-
-    if ((level_len - len(masked_1 <= 1) | (level_len - len(masked_2) <= 1))):
         count2 += 1
-        print("safe", level)
-
-    print(level, differences, level_len, masked_1, masked_2)
+        print(level, "safe")
+    elif (((sum(diff2 < 1) + sum(diff2 > 3) + sum(diff2 == 0) <= 1)) | (sum(diff2 > -1) + sum(diff2 < -3) + sum(diff2 == 0) <= 1)):
+        count2 += 1
+        print(level, "safe, rmv")
+    
 
 print(count, count2)
