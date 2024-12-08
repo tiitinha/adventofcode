2e2ffb4ed0e4ec63e6971d3c7705a980e2f167b0
Harri Tiitinen <htiitinen94@gmail.com>
2024-12-08 14:46:37 +0200

    Day 7 done

--- AoC24/day6/python/day6.py
+++ AoC24/day6/python/day6.py
@@ -2,7 +2,7 @@ import numpy as np
 
 rot_right = np.array([[0, 1], [-1, 0]])
 
-with open('../input.txt') as f:
+with open('../test.txt') as f:
     map_input = np.array([[z for z in y] for y in [x.strip() for x in f.readlines()]])
 
 width = len(map_input[0]) - 1
@@ -10,6 +10,14 @@ height = len(map_input) - 1
 
 guard_location = np.argwhere(map_input == '^')[0]
 obstacles = np.argwhere(map_input == '#')
+
+def check_obstacle(location, direction, visited):
+    next_loc = location + direction
+
+    if (next_loc[0], next_loc[1]) in (visited):
+        return True
+
+    return False
 
 def can_move_forward(location, direction):
     potential_next = location + direction
@@ -34,19 +42,34 @@ def can_turn_right(location, direction):
 
     return False
 
+def is_going_out(guard_location, direction):
+    next_location = guard_location + direction
+
+    return (next_location[0] < 0) | (next_location[0] > height) | (next_location[1] < 0) | (next_location[1] > width)
+
 direction = np.array([-1, 0])
 visited = set()
 visited.add((guard_location[0], guard_location[1]))
+visited_obstacles = set()
+count = 0
 
 while True:
-    if can_move_forward(guard_location, direction):
+    if is_going_out(guard_location, direction):
+       break 
+    elif can_move_forward(guard_location, direction):
         guard_location += direction
     elif can_turn_right(guard_location, direction):
+        visited_obstacle = guard_location + direction
         direction = np.dot(rot_right, direction)
         guard_location += direction
-    else:
-        break
+
+        visited_obstacles.add((visited_obstacle[0], visited_obstacle[1]))
+
+    if check_obstacle(guard_location, direction, visited_obstacles):
+        print(guard_location, direction)
+        count += 1
 
     visited.add((guard_location[0], guard_location[1]))
 
 print(len(visited))
+print(count)
--- AoC24/day7/python/day7.py
+++ AoC24/day7/python/day7.py
@@ -1,1 +1,37 @@
+import math
 
+def numcat(a,b):
+    return int(math.pow(10,(int(math.log(b,10)) + 1)) * a + b)
+
+with open('../input.txt') as f:
+    calibrations = [x.strip().split(': ') for x in f.readlines()]
+
+def evaluate(inputs):
+    interim_results = set()
+    interim_results.add(inputs[0])
+
+    for input in inputs[1:]:
+        next_results = set()
+        for res in interim_results:
+            plus = res + input
+            mul = res * input
+            concat = int(str(res) + str(input))
+            next_results.add(plus)
+            next_results.add(mul)
+            next_results.add(concat)
+
+        interim_results = next_results
+
+    return interim_results
+
+total = 0
+for calibration in calibrations:
+    result = int(calibration[0])
+    inputs = [int(x) for x in calibration[1].split(' ')]
+
+    possible_results = evaluate(inputs)
+
+    if result in possible_results:
+        total += result
+
+print(total)
